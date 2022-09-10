59e7a794b40cf0afed2dbf728c1c86362364b8c4
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-10 08:39:01 +0530

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k;
+            for (i = 1; i <= 4; i++)
+            {
+                for (j = i; j <= 4; j++)
+                {
+                    Console.Write("");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+
+            }
+            Console.ReadLine();
+
+    }   }
+}
